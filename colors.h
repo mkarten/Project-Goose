454c4fe6571b3f4b054dc73af73dc93eb1d1ef4d
Luca Morgado <luca.morgado.pro@gmail.com>
2023-08-23 23:41:05 +0200

    added colors.h for color references

---
+++ colors.h
@@ -0,0 +1,19 @@
+#pragma once
+
+// colors
+#define White 0x0F
+#define Black 0x00
+#define Blue 0x01
+#define Green 0x02
+#define Cyan 0x03
+#define Red 0x04
+#define Magenta 0x05
+#define Brown 0x06
+#define LightGray 0x07
+#define DarkGray 0x08
+#define LightBlue 0x09
+#define LightGreen 0x0A
+#define LightCyan 0x0B
+#define LightRed 0x0C
+#define LightMagenta 0x0D
+#define Yellow 0x0E
--- kernel.c
+++ kernel.c
@@ -1,25 +1,9 @@
 #include "stdint.h"
+#include "colors.h"
+
 void _cdecl c_entry_(){
     c_kernel_();
 }
-
-// colors
-#define White 0x0F
-#define Black 0x00
-#define Blue 0x01
-#define Green 0x02
-#define Cyan 0x03
-#define Red 0x04
-#define Magenta 0x05
-#define Brown 0x06
-#define LightGray 0x07
-#define DarkGray 0x08
-#define LightBlue 0x09
-#define LightGreen 0x0A
-#define LightCyan 0x0B
-#define LightRed 0x0C
-#define LightMagenta 0x0D
-#define Yellow 0x0E
 
 void wait(uint32_t time) {
     for (int i = 0; i < time; i++) {
